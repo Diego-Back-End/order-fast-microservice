4a4ff50ba3e24c57713cd382c6bda9b31041c70f
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-20 09:42:37 -0400

    se modifico model usuario

--- Usuario/Usuario/src/main/java/com/example/Usuario/model/Usuario.java
+++ Usuario/Usuario/src/main/java/com/example/Usuario/model/Usuario.java
@@ -2,45 +2,26 @@ package com.example.Usuario.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "usuarios")
+@Data 
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
 
     private String email;
 
     // Constructor vacío obligatorio para JPA
-    public Usuario() {}
-
-    public Usuario(String nombre, String email) {
-        this.nombre = nombre;
-        this.email = email;
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+   
 }
