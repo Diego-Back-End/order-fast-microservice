86c8a4ab18c72167756b53a66cc75286bd2ea3d1
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-20 09:50:36 -0400

    se modifico la forma de construir los getters y setter y contructores

--- Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
+++ Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
@@ -1,7 +1,12 @@
 package com.example.Restaurante.model;
 
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "restaurantes")
 public class Restaurante {
@@ -16,40 +21,5 @@ public class Restaurante {
 
     private String telefono;
 
-    public Restaurante() {}
 
-    public Restaurante(String nombre, String direccion, String telefono) {
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
