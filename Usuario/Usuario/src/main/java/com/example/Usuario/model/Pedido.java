dcfa5bfbb5c9e1a8d2d3b25d54e7cbc0cbddcd3d
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-20 19:36:32 -0400

    ingresado constructores, getter y setter en la clase pedido

--- Usuario/Usuario/src/main/java/com/example/Usuario/model/Pedido.java
+++ Usuario/Usuario/src/main/java/com/example/Usuario/model/Pedido.java
@@ -1,5 +1,28 @@
 package com.example.Usuario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pedidos")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true)
+    private LocalDate fecha;
+
+    private double total;
+
+    @Column(nullable = false)
+    private Long usuarioId; // Relación básica con Usuario
 
 }
