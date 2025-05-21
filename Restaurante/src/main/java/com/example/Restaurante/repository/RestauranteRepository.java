6c5d9835491dfa973d3e52f6acd40817c1f05160
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-21 02:03:33 -0400

    documentacion lista

--- Restaurante/src/main/java/com/example/Restaurante/repository/RestauranteRepository.java
+++ Restaurante/src/main/java/com/example/Restaurante/repository/RestauranteRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Restaurante.model.Restaurante;
-
-@Repository
+/**
+ * Esta interfaz se encarga de la comunicación con la base de datos para la entidad Usuario.
+ * Gracias a JpaRepository, ya trae muchas funciones listas como guardar, buscar, eliminar, etc.
+ */
+@Repository  // Le dice a Spring que esta clase es un repositorio (una capa de acceso a datos)
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 }
+//jpa traera todos los metodos necesarios como
+// save, findbyid, findall, delete, etc
 
