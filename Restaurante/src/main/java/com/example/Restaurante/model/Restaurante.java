054a2d94de37c9dca5d81a188ece17e443cfe774
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-21 02:03:14 -0400

    documentando codigo para no llorar despues

--- Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
+++ Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
@@ -4,25 +4,25 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "restaurantes")
+@Data  // Anotación de Lombok que genera todos los métodos estándar como getters y setters
+@NoArgsConstructor // Genera un constructor con todos los atributos
+@AllArgsConstructor // Genera un constructor vacío, requerido por JPA
+@Entity // Marca esta clase como una entidad JPA que se mapeará a una tabla en la base de datos
+@Table(name = "restaurantes")  // Define el nombre de la tabla en la base de datos como "restaurante"
 public class Restaurante {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //indica que este atributo es la clave priamria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // El ID se autogenera
 
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false) //  no puede ser nulo
     private String nombre;
 
-    @Column(nullable = false)
+    @Column(nullable = false) //  no puede ser nulo
     private String direccion;
 
-    @Column(nullable = false)
+    @Column(nullable = false) // no puede ser nulo
     private String telefono;
 
 
