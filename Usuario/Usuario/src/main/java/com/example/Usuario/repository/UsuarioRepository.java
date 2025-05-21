a2841df7ee7a73d69e2982532a31d8aa79679050
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-21 02:04:46 -0400

    doc para repository

--- Usuario/Usuario/src/main/java/com/example/Usuario/repository/UsuarioRepository.java
+++ Usuario/Usuario/src/main/java/com/example/Usuario/repository/UsuarioRepository.java
@@ -1,11 +1,16 @@
 package com.example.Usuario.repository;
 
-
+//importaciones para trabajar con repositorios en spring y jpa
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Usuario.model.Usuario;
 
-@Repository
+/**
+ * Esta interfaz se encarga de la comunicación con la base de datos para la entidad Usuario.
+ * Gracias a JpaRepository, ya trae muchas funciones listas como guardar, buscar, eliminar, etc.
+ */
+@Repository // esta clase es un repositorio (una capa de acceso a datos)
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
