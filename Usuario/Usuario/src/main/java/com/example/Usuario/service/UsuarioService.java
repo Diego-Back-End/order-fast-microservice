cd6d69ee8c7dbbc5a6c6c9e4d41f44f30e97ab6a
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-21 02:05:00 -0400

    agregando documentacion en service usuario

--- Usuario/Usuario/src/main/java/com/example/Usuario/service/UsuarioService.java
+++ Usuario/Usuario/src/main/java/com/example/Usuario/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.example.Usuario.service;
 
+//en jarkarta se manejara la base de datos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +16,28 @@ import java.util.Optional;
 @Transactional
 public class UsuarioService {
 
-    @Autowired
+    @Autowired//se utilizara automaticamente el repositorio para utilizarlo en esta clase
     private UsuarioRepository usuarioRepository;
 
+    //traera los usuarios existentes de la base de datos
     public List<Usuario> obtenerTodos() {
         return usuarioRepository.findAll();
     }
 
+    //traera los usuarios que existen en la base de datos 
+    //los podremos llamar por su id
     public Optional<Usuario> obtenerPorId(Long id) {
         return usuarioRepository.findById(id);
     }
 
+
+    //crea un nuevo usuario y sera guardado automaticamente en la base de datos
     public Usuario crearUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
     
+    //actualiza un usuario existente (id) actualiza (nombre,email) 
+    //si no retorna nulo
     public Usuario actualizarUsuario(Long id, Usuario usuarioActualizado) {
         // return usuarioRepository.save(usuarioActualizado);
         return usuarioRepository.findById(id).map(usuario -> {
@@ -39,9 +47,13 @@ public class UsuarioService {
         }).orElse(null);
     }
 
+
+    //se elimina un usuario por su id
     public void eliminarUsuario(Long id) {
         usuarioRepository.deleteById(id);
     }
+
+    //guardara todo de una sola vez
     public List<Usuario> guardarTodos(List<Usuario> usuarios) {
         return usuarioRepository.saveAll(usuarios);
     }
