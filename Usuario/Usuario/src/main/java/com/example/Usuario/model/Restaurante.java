3de19243803d8921da078bd4fd2f1d5a0c642ca9
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-05-20 20:20:31 -0400

    se agrefoe el pedido service en usuario para la relacion de uno a muchos

--- Usuario/Usuario/src/main/java/com/example/Usuario/model/Restaurante.java
+++ Usuario/Usuario/src/main/java/com/example/Usuario/model/Restaurante.java
@@ -1,5 +1,29 @@
 package com.example.Usuario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "restaurantes")
 public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false) // esto no puede ser nulo
+    private String nombre;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    @Column(nullable = false)
+    private String telefono;
+
 
 }
